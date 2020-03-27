0f2c9ab852a90e8a4ab8d7fe9f11ac5709de866e
dimahnatk <dimahnatkivskiy@gmail.com>
2020-03-27 15:17:00 +0200

    Updates

---
+++ precache-manifest.bff60456d2781a67e0b53a7f80a71833.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a246f3d4676f4008af1502a1adef65",
+    "url": "/MovieHub/index.html"
+  },
+  {
+    "revision": "ef454c4e68ff7cb7aaf1",
+    "url": "/MovieHub/static/css/main.16a193c5.chunk.css"
+  },
+  {
+    "revision": "e9b51a5b86965a208c60",
+    "url": "/MovieHub/static/js/2.f4924063.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/MovieHub/static/js/2.f4924063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef454c4e68ff7cb7aaf1",
+    "url": "/MovieHub/static/js/main.4c071d1c.chunk.js"
+  },
+  {
+    "revision": "33b1d2ca3beea9c7433c",
+    "url": "/MovieHub/static/js/runtime-main.121eb934.js"
+  },
+  {
+    "revision": "6965e9ef1f7f5bf89f8fa65662f44ceb",
+    "url": "/MovieHub/static/media/404.6965e9ef.gif"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "/MovieHub/static/media/Nunito-Bold.1cd294a7.ttf"
+  },
+  {
+    "revision": "6a4fadd273f85b2ae8c6ffadbcf7beec",
+    "url": "/MovieHub/static/media/Nunito-Light.6a4fadd2.ttf"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/MovieHub/static/media/Nunito-Regular.d2e691bc.ttf"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/MovieHub/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "1cb4e224d40132298361a27f8182e5f7",
+    "url": "/MovieHub/static/media/empty-list.1cb4e224.png"
+  },
+  {
+    "revision": "3bbca59ffd76ef952ef9fe0d5fd17dce",
+    "url": "/MovieHub/static/media/icon-movie.3bbca59f.png"
+  }
+]);
